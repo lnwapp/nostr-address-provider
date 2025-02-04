69a6c2ed72896defa82445e8b1666402f9f78dac
pcwd <41215821+pcwd@users.noreply.github.com>
2025-02-04 10:53:32 +1000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default {
   registrationDisabled: false,
 
   // site slogan
-  slogan: "Personalised nostr addresses.",
+  slogan: "Personalized nostr addresses.",
 
   // your donation LNURL
   donationLNUrl:
